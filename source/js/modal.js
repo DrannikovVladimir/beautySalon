b02da189e5e4c50019a89c621ee41132220d2b4a
Vladimir Drannikov <krowen@inbox.ru>
2020-01-14 14:26:57 +0600

    Добавил валидацию полей формы

--- source/js/modal.js
+++ source/js/modal.js
@@ -5,7 +5,10 @@ const closeCallback = modalCallback.querySelector('.modal__button-close');
 const buttonCallback = document.querySelector('.page-header__callback');
 const modalCheckin = document.querySelector('.modal--checkin');
 const closeCheckin = modalCheckin.querySelector('.modal__button-close');
-const buttonCheckin = Array.from(document.querySelectorAll('.button--checkin'));
+const buttonCheckin = Array.from(document.querySelectorAll('.checkin'));
+
+const nameInput = Array.from(document.querySelectorAll('.input-name'));
+const phoneInput = Array.from(document.querySelectorAll('.form-modal__input[type=tel]'));
 
 const onEscModalCallbackPress = (evt) => {
   if (evt.keyCode === 27) {
@@ -58,5 +61,50 @@ buttonCheckin.forEach((button) => {
   closeCheckin.addEventListener('click', (evt) => {
     evt.preventDefault();
     modalCheckinClose();
+  });
+});
+
+phoneInput.forEach((phone) => {
+  phone.addEventListener('focus', (evt) => {
+    phone.value = '+7 ( ';
+  });
+
+  phone.addEventListener('input', (evt) => {
+    let currentLetter = phone.value.length;
+
+    if (currentLetter === 8) {
+      phone.value += ' ) ';
+    }
+    if (currentLetter === 14) {
+      phone.value += ' - ';
+    }
+    if (currentLetter === 19) {
+      phone.value += ' - ';
+    }
+  });
+
+  phone.addEventListener('blur', (evt) => {
+    const phoneValue = phone.value;
+
+    if (phoneValue.length < 10) {
+      phone.value = '';
+    }
+
+    if (phone.validity.patternMismatch) {
+      phone.setCustomValidity('Номер телефона должен быть в формате +7 ( XXX ) XXX - XX - XX');
+    } else {
+      phone.setCustomValidity('');
+    }
+  });
+});
+
+nameInput.forEach((name) => {
+  name.addEventListener('blur', (evt) => {
+    console.log(name);
+    if (name.validity.patternMismatch) {
+      name.setCustomValidity('Имя может содержать только буквы');
+    } else {
+      name.setCustomValidity('');
+    }
   });
 });
